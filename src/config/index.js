2d4db44366e4c95662acc93b3d7e07e59e1dc7dd
ledoanh96 <ledoanh96@gmail.com>
2019-05-13 17:25:34 -0400

    second commit

--- src/config/index.js
+++ src/config/index.js
@@ -4,8 +4,8 @@ module.exports = {
     "https://maps.googleapis.com/maps/api/place/autocomplete/json?types:(cities)",
   placeDetailBaseURL:
     "https://maps.googleapis.com/maps/api/place/details/json?",
-  weatherBaseURL: "http://api.apixu.com/v1/current.json?",
-  forecastBaseURL:"http://api.apixu.com/v1/forecast.json?",
+  weatherBaseURL: "https://api.apixu.com/v1/current.json?",
+  forecastBaseURL:"https://api.apixu.com/v1/forecast.json?",
   googleKey: "AIzaSyCXjMCgV4xSa1GV_qZijGu7ZJHbdNLKEBU",
   weatherKey: "3f396c019636440fbc663145182412"
 };
